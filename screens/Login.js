269abe9a82b1b35dbf0c06edbf2bfc48bdefa7d4
IrfanMuzamil <Irfanullahchagharzai@gmail.com>
2024-10-27 23:27:06 +0500

    Changes

--- screens/Login.js
+++ screens/Login.js
@@ -12,7 +12,6 @@ import { login } from "../Services/api";
 const Login = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
   const handleLogin = async () => {
     try {
       await login({ email, password });
